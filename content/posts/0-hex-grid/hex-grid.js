b846f87081e480161fcfa68012c0898514922114
Andrey Potapov <andrey.nautilus@yandex.ru>
2025-04-24 01:20:04 +0200

    fixup! post: hex grid

--- content/posts/0-hex-grid/hex-grid.js
+++ content/posts/0-hex-grid/hex-grid.js
@@ -9,6 +9,10 @@ class Point {
     }
 }
 
+function distanceSq(p1, p2) {
+    return (p1.row - p2.row) ** 2 + (p1.col + 0.5 * (p1.row % 2) - p2.col - 0.5 * (p2.row % 2)) ** 2;
+}
+
 function randomInt(max) {
     return Math.floor(max * Math.random());
 }
@@ -74,51 +78,3 @@ class HexGrid {
         return candidates.filter((p) => this.cell(p) == undefined);
     }
 };
-
-function generateInitialPoints(grid, amount) {
-    let result = [];
-    for (let i = 0; i < amount; ++i) {
-        let point = randomIntPoint(grid.rows_count, grid.cols_count);
-        while (result.find((p) => point.equals(p)) != undefined) {
-            // re-generate in case of duplicates
-            point = randomIntPoint(grid.rows_count, grid.cols_count);
-        }
-        result.push(point);
-    }
-    return result;
-}
-
-function generateMap(grid, number_of_areas) {
-    // generate starting points
-    let areas = [];  // list of "list of points + color_index"
-    const initialPoints = generateInitialPoints(grid, number_of_areas);
-    initialPoints.forEach((p, idx) => {
-        areas.push([[p, idx]]);
-        grid.setCell(p, idx);
-    });
-
-    // apply flood fill
-    let expandable_points_count = areas.reduce((acc, points) => acc + points.length, 0)
-    let iter = 0;
-    while (expandable_points_count > 0) {
-        // choose area
-        const area_idx = randomInt(areas.length);
-        let area = areas[area_idx];
-
-        // pick point in the area to expand
-        let point_idx = Math.floor(area.length * Math.random());
-        const [point, color_idx] = area[point_idx];
-        let candidates = grid.getCandidates(point);
-        const cand = candidates[randomInt(candidates.length)];
-        area.push([cand, color_idx]);
-        grid.setCell(cand, color_idx);
-
-        // clean up points and areas
-        // TODO: very inefficient, but works
-        areas = areas
-            .map((area) => area.filter(([point, color_idx]) => grid.getCandidates(point).length > 0))
-            .filter((area) => area.length > 0);
-
-        expandable_points_count = areas.reduce((acc, points) => acc + points.length, 0);
-    }
-}
